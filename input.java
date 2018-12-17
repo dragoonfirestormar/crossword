3127a3f89d42ae7c4f826c4e5b83ee351e0ce130
dragoonfirestormar <harshmohan813@gmail.com>
2018-12-17 22:46:10 +0530

    Update input.java

--- input.java
+++ input.java
@@ -1,12 +1,28 @@
+/*
+Will be getting following inputs:-
+*Dimention Of DDA
+*Filled DDA
+*Word(s) To Execute
 
+The following data will be given out:-
+*no. of row (int)
+*no. of column (int)
+*no. of words to find (int)
+*words to find (ArrayList)
+*crossword (Arrays)
+*/
 package crossword;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
+
 public class input
 {
     public Scanner y = new Scanner(System.in);
     public static int row;
-    public static int column ;
+    public static int column;
+    public static int noOFWORDS;
+    public ArrayList<String> words = new ArrayList<String>();
     public input()
     {
 
@@ -15,20 +31,24 @@ public class input
     {
         input ex = new input();
         ex.dimention();
-        String[][] lmfao = ex.filling();
-        System.out.println(Arrays.deepToString(lmfao));
+        word(); //getting words
+        String[][] FilledArray = ex.filling(); //getting filled array
+        //System.out.println(Arrays.deepToString(FilledArray)); to test the filled array
+        //System.out.println(words); to test the given words
     }
 
     
     public void dimention()
     {
         System.out.print("Enter the number of row : ");
-        row = y.nextInt();
+        try{row = y.nextInt();}
+        catch (Exception e) { System.out.println("Warning: Enter The Number (Integer), Not A String"); }
         System.out.print("\nEnter the number of column : ");
-        column = y.nextInt();
+        try{column = y.nextInt();}
+        catch (Exception e) { System.out.println("Warning: Enter The Number (Integer), Not A String"); }
     }
     
-    public String[][] creator ()
+    public String[][] creator()
     {
         String[][] crossword = new String [column][row];
         return crossword;
@@ -41,7 +61,6 @@ public class input
         {
             for(int b=0 ; b<row ; b++)
             {
-                
                 System.out.print("\nEnter The Element Of Column : "+(a+1)+" and Row : "+(b+1)+" : ");
                 filled[a][b] = String.valueOf((y.next()).charAt(0));
             }
@@ -49,5 +68,18 @@ public class input
         return filled;  
     }
     
+    public void word() 
+    {
+        System.out.println("Enter The Number Of Words To Find");
+        try{
+        noOFWORDS = y.nextInt(); }
+        catch (Exception e) { System.out.println("Warning: Enter The Number (Integer), Not A String"); }
+        for(int c = 0 ; c<noOFWORDS ; c++)
+        {
+            System.out.println("Enter The Word To Find");
+            
+            words.add(y.next());
+        }
+    }
     
 }
